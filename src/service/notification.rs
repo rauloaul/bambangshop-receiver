9b528022b2f77705ddf3ef7ea45abdef4d66ab19
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-04-17 18:06:40 +0700

    Fix: Typo and Error 2

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -48,7 +48,7 @@ impl NotificationService {
 
     pub fn subscribe(product_type: &str) -> Result<SubscriberRequest> {
         let product_type_clone = String::from(product_type);
-        return thread ::spawn(move || Self::subscriber_request(product_type_clone))
+        return thread ::spawn(move || Self::subscribe_request(product_type_clone))
             .join().unwrap();
     }
 
